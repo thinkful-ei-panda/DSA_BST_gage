ea2c915f02d684a338e93206cb85e78756203677
Gage <gagemigan@gmail.com>
2020-08-25 11:20:28 -0400

    added remove

--- binary_search_tree/binarySearchTree.js
+++ binary_search_tree/binarySearchTree.js
@@ -67,5 +67,75 @@ class BinarySearchTree {
 			throw new Error('key Error');
 		}
 	}
+	remove(key){
+		if(this.key === key){
+			if(this.left && this.right){
+				const successor = this.right._findMin();
+				this.key = successor.key;
+				this.value = successor.value;
+				successor.remove(successor.key);
+			}
+			/*If the node only has a left child,
+			 * then you replace the node with its left child*/
+			else if(this.left){
+				this._replaceWith(this.left);
+			}
+			/*And somilary if the node only has a right child 
+			 * then you replace it with its right child */ 
+			else if(this.right){
+				this._replaceWith(this.right);
+			}
+			/* If the node has no children then simply remove it and
+			 * references to it by
+			 * calling "this._replaceWith(null)"*/
+			else {
+				this._replaceWith(null)
+			}
+		}
+		else if( key < this.key && this.left) {
+			this.left.remove(key);
+		}
+		else if( key > this.key && this.right) {
+			this.right.remove(key)
+		}
+		else{
+			throw new Error('key Error')
+		}
+	}
+	_replaceWith(node){
+		if (this.parent){
+			if (this == this.parent.left) {
+				this.parent.left = node;
+			}
+			else if(this == this.parent.right){
+				this.parent.right = node;
+			}
+
+			if(node){
+				node.parent = this.parent;
+			}
+		}
+		else {
+			if (node) {
+				this.key = node. key;
+				this.value = node.value;
+				this.left = node.left;
+				this.right = node.right;
+			}
+			else {
+				this.key = null;
+				this.value = null;
+				this.left = null;
+				this.right = null;
+			}
+		}
+	}
+	_findMin(){
+		if(!this.left){
+			return this;
+		}
+		return this.left._findMin();
+	}
+	
 }
 module.export = BinarySearchTree
